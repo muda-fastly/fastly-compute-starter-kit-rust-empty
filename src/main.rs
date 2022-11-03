03f6465d3dcbe1ee0c3949bfdefe10f8141c65d1
muda-fastly <mmohammed@fastly.com>
2022-11-02 23:59:25 -0400

    making it work
    - code is the same as ngwaf code.
    - proxying everything to the origin as it is recevied
    - proxying back everything as recevied from the origin

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,40 @@ use fastly::http::StatusCode;
 use fastly::{Error, Request, Response};
 
 #[fastly::main]
-fn main(_req: Request) -> Result<Response, Error> {
-    Ok(Response::from_status(StatusCode::OK))
+fn main(req: Request) -> Result<Response, Error> {
+    // let mut req = Request::from_client();
+    //
+    // request lands on compute service 1
+    // add header chain-status and send to compute service 2
+    // compute service 2 routes request back to compute service 1
+    // if chain-status header exists with a value of 1 then do complicated routing logic
+    //
+
+    // baby steps
+    // if chain-status header doesnt exist create it and send it httpbin to be reflected back in response
+
+    const HBIN: &str = "OHTTP";
+
+	// let c_status = req.get_header_str("chain-status")
+	// 							.unwrap_or("")
+	// 							.to_string();
+    
+    for name in req.get_header_names() {
+        println!("saw header: {:?}", name);
+    }
+
+    // req.set_header("host", "httpbin.org");
+    
+    // if c_status == "" {
+        // let request = Request::get(
+        // let bin_resp = Request::get("https://httpbin.org/headers")
+        // .with_pass(true)
+        // .send(HBIN)?;
+
+        return Ok(req
+            .with_pass(true)
+            .send(HBIN)?);
+
+        // Ok(bin_resp)
+
 }
